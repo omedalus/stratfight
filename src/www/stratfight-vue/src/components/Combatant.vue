0522a5a8f19f19f5e94bd840c793893a3d7c5583
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2019-05-11 17:25:10 -0400

    Adding vuex support.

---
+++ src/www/stratfight-vue/src/components/Combatant.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+// The @Component decorator indicates the class is a Vue component
+@Component
+export default class CombatantComponent extends Vue {
+  // Initial data can be declared as instance properties
+  message: string = 'Hello!'
+
+  // Component methods can be declared as instance methods
+  onClick (): void {
+    window.alert(this.message)
+  }
+}
+</script>
+
+<style>
+
+</style>
