626040eaf224dfc5ebcb24bb88238b8003f84546
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2019-05-11 22:19:52 -0400

    Amanda wants to cuddle

--- src/www/stratfight-vue/src/store.ts
+++ src/www/stratfight-vue/src/store.ts
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+const sampleArmors = require('./assets/sampledata/armors.json');
+const sampleWeapons = require('./assets/sampledata/weapons.json');
+const sampleCombatants = require('./assets/sampledata/combatants.json');
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    combatants: []
+    combatants: sampleCombatants,
+    weapons: sampleWeapons,
+    armors: sampleArmors
   },
   mutations: {
 
