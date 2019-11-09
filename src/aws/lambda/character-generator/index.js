52f2d580ee69ee436efd6151736e77e66e139b85
Mikhail Voloshin <mvol@mightydatainc.com>
2019-11-09 17:08:09 -0500

    Saving some initial work on the character generator.

--- src/aws/lambda/character-generator/index.js
+++ src/aws/lambda/character-generator/index.js
@@ -1,8 +1,36 @@
+const SCORE_SCALE = 25;
+
+const generateCharacter = () => {
+  const retval = {
+    attributes: {
+      prowess: Math.floor(Math.random() * SCORE_SCALE) + 1,
+      toughness: Math.floor(Math.random() * SCORE_SCALE / 5) + 1,
+      speed: Math.floor(Math.random() * SCORE_SCALE / 5) + 1,
+    },
+    items_skills_and_effects: {
+
+    },
+    motivations: {
+
+    },
+    condition: {
+      damage: 0,
+      injury: 0
+    },
+    allocations: {
+
+    }
+  };
+  return retval;
+};
+
+
 const handler = async (event) => {
-  // TODO implement
+  const ch = generateCharacter();
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify(ch),
   };
   return response;
 };
