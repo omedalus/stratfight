c301989883de05382e75b0ed3848d9390d2c0a7d
Mikhail Voloshin <mvol@mightydatainc.com>
2019-11-10 00:42:34 -0500

    Making up some bonuses

--- src/aws/lambda/character-generator/index.js
+++ src/aws/lambda/character-generator/index.js
@@ -1,28 +1,78 @@
-const SCORE_SCALE = 25;
+const SCORE_SCALE = 30;
 
 const generateCharacter = () => {
   const retval = {
+    id: "",
+    client_id: "",
+    game_id: "",
+
     attributes: {
       prowess: Math.floor(Math.random() * SCORE_SCALE) + 1,
       toughness: Math.floor(Math.random() * SCORE_SCALE / 5) + 1,
       speed: Math.floor(Math.random() * SCORE_SCALE / 5) + 1,
     },
-    bonuses: {
-
-    },
-    motivations: {
-
-    },
+    bonuses: [],
     condition: {
       damage: 0,
       injury: 0
     },
     allocations: {
-
+      threaten: 0,
+      guard: 0,
+      attack: 0,
+      defend: 0,
+      pull: 0,
+      dash: 0
     }
   };
   return retval;
 };
+
+const generateBonuses = () => {
+
+};
+
+const bonuspool = [
+  {
+    name: "Bruiser",
+    description: "When an attack connects, it inflicts possible additional points of Damage.",
+    attributes: ["skill"],
+    stats: {
+      damage: 5
+    }
+  }, {
+    name: "Sparmaster",
+    description: "You get 5 free points of Pull.",
+    attributes: ["skill"],
+    stats: {
+      pull: 5
+    }
+  }, {
+    name: "Greased",
+    description: "You get 5 free points of Dash. Note that this does not help you in Pursuit.",
+    attributes: ["status"],
+    stats: {
+      dash: 5
+    }
+  }, {
+    name: "Gun",
+    description: "You have a handgun, which gives you large bonuses to Damage and to Pursuit (because the opponent has to outrun your aim).",
+    attributes: ["item", "weapon", "hand", "ranged"],
+    stats: {
+      damage: 20,
+      pursue: 20
+    }
+  }, {
+    name: "Sword",
+    description: "You have a sword, which gives you large bonuses to Damage. It also gives you a Defense bonus if the opponent is also armed with a melee weapon.",
+    attributes: ["item", "weapon", "hand", "melee"],
+    stats: {
+      damage: 20,
+      pursue: 20
+    }
+  }
+];
+
 
 
 const handler = async (event) => {
